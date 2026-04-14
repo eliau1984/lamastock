0652850ae02828d0d74d7001a0a1e1d6af1e328c
eliau1984 <eliau555@walla.com>
2026-04-14 17:34:00 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,22 @@
-<script>
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js');
-  }
-</script>
+const CACHE_NAME = 'lamastock-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './logo.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting(); // מכריח התקנה מיידית
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim()); // משתלט על הדף מיד
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
