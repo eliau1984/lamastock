661cab58a0942debca896e1fae76e0aaea3f7267
eliau1984 <eliau555@walla.com>
2026-04-15 15:52:52 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,8 @@
-const CACHE_NAME = 'lamastock-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './logo.png',
-  './manifest.json'
-];
-
 self.addEventListener('install', (e) => {
-  self.skipWaiting(); // מכריח התקנה מיידית
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim()); // משתלט על הדף מיד
+  console.log('Service Worker: Installed');
 });
 
 self.addEventListener('fetch', (e) => {
+  // גורם לאפליקציה לעבוד גם כשיש בעיות רשת רגעיות
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
